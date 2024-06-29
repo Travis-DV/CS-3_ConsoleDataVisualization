9bcf774da586b1c5369256fbcc6de9bcd32bdcfb
Travis-DV <tfindley2@my.stlcc.edu>
2024-06-29 15:25:39 -0500

    Added code

---
+++ ConsoleDataVisualization/ConsoleDataVisualization/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Validator;
+using Utilitys;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace ConsoleDataVisualization
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int numberOfPoints;
+            int[] data = new int[1];
+
+            int OneOrTwo = Validator.Validator.OneOrTwo("Enter Data", "Use Random Data", "Do you want to");
+
+            if (OneOrTwo == 1)
+            {
+                data = Validator.Validator.GetIntArray("Enter Data");
+            }
+            else if (OneOrTwo == 2)
+            {
+                numberOfPoints = Validator.Validator.GetInt("How many data points do you want?");
+                data = new int[numberOfPoints];
+                for (int i = 0; i < numberOfPoints; i++)
+                {
+                    data[i] = RandomNumber.Between();
+                }
+            }
+
+            GenerateBarChart(data);
+        }
+
+        public static void GenerateBarChart(int[] input)
+        {
+            foreach (int item in input)
+            {
+                Console.Write($"{item.ToString()}: ");
+                Console.WriteLine("".PadLeft(item, '#'));
+            }
+        }
+    }
+}
+
---
+++ ConsoleDataVisualization/ConsoleDataVisualization/Utilitys.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Security.Cryptography;
+
+
+namespace Utilitys
+{
+    public static class RandomNumber
+    {
+        private static readonly RNGCryptoServiceProvider _generator = new RNGCryptoServiceProvider();
+        public static int Between(int minimumValue = 0, int maximumValue = 100)
+        {
+            if (minimumValue == 0 && maximumValue == 0) { return 0; }
+            byte[] randomNumbers = new byte[1];
+            _generator.GetBytes(randomNumbers);
+            double asciiValueOfRandomCharacter = Convert.ToDouble(randomNumbers[0]);
+            double multiplier = Math.Max(0, (asciiValueOfRandomCharacter / 255d) - 0.00000000001d);
+            int range = (maximumValue - 1) - minimumValue;
+            double randomValueInRange = Math.Floor(multiplier * range);
+            return (int)(minimumValue + randomValueInRange);
+        }
+    }
+
+    public static class Extensions
+    {
+        public static T pop<T>(this List<T> list, int index = -1)
+        {
+            if (list.Count == 0) { throw new InvalidOperationException("Cannot pop from an empty list"); }
+
+            if (index == -1) { index = list.Count - 1; }
+
+            T temp = list[index];
+            list.RemoveAt(index);
+            return temp;
+        }
+
+        public static int ToInt(this bool value)
+        {
+            return value ? 1 : 0;
+        }
+    }
+}
